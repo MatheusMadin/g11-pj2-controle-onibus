2d56d6533378f39bc9b55a91b339d7e16f6cb6c7
ARTHYrobo <ARTHY.robot1@gmail.com>
2023-10-01 21:47:03 -0300

    renovar banco (so pra ter certeza)

--- bancooni.sql
+++ bancooni.sql
@@ -7,6 +7,45 @@ CREATE TABLE usuario (
     nome VARCHAR(200) NOT NULL,
     email VARCHAR(200)
 );
+/* primeiro esse*/
+
+  create table onibus (
+    idONI int auto_increment primary key,
+	modelo varchar(100) NOT NULL,
+    placa varchar (7) NOT NULL,
+    numeracao varchar(6) NOT NULL
+ );
+ /* segundo esse*/ 
+ 
+  create table motorista (
+    idMT  int    auto_increment,
+    cpf    varchar (14) not null,
+    codigo_carteira varchar(24),
+    nome   varchar(200) not null,
+    email  varchar(200),
+    telefone varchar(17),
+    nascimento date,
+	uf 	  varchar(2),
+    cep   varchar(12) not null,
+    cidade varchar(200),
+    bairro varchar(200),
+    rua    varchar(200),
+    PRIMARY KEY (idMT, cpf, codigo_carteira)
+ );
+ /* terceiro esse*/
+ 
+  create table linha (
+	idLI int auto_increment primary key,
+    lin_idMT int,
+    lin_idONI int,
+	local_inicio varchar(200) NOT NULL,
+    local_fim varchar(200) NOT NULL,
+    horario_inicio datetime NOT NULL,
+    horario_fim  datetime NOT NULL,
+    constraint fk_motorista_linha foreign key (lin_idMT) references motorista(idMT),
+    constraint fk_onibus_linha foreign key (lin_idONI) references onibus(idONI)
+ );
+ /* quarto esse*/
  
  create table cliente (
 	usr_idUS INT,
@@ -26,6 +65,7 @@ CREATE TABLE usuario (
     CONSTRAINT fk_usuario_cliente foreign key (usr_idUS) references usuario (idUS),
     constraint fk_nome_cliente foreign key (nome) references usuario(nome)
  );
+ /* quinto esse*/
  
  create table cartao (
 	idCA int auto_increment primary key,
@@ -35,99 +75,31 @@ CREATE TABLE usuario (
     saldo INT NOT NULL,
     constraint fk_cartao_cliente foreign key (crt_idCL) references cliente (idCL)
  );
+ /* sexto esse*/
  
+
  
  create table acompanhante (
 	nome varchar(200) not null,
     acp_idCA int NOT NULL,
     constraint fk_acompanhante_cartao foreign key (acp_idCA) references cartao (idCA)
 );
- 
- create table onibus (
-    idONI int auto_increment primary key,
-	modelo varchar(100) NOT NULL,
-    placa varchar (7) NOT NULL,
-    numeracao varchar(6) NOT NULL
- );
- 
- create table motorista (
-    idMT  int    auto_increment,
-    cpf    varchar (14) not null,
-    codigo_carteira varchar(24),
-    nome   varchar(200) not null,
-    email  varchar(200),
-    telefone varchar(17),
-    nascimento date,
-	uf 	  varchar(2),
-    cep   varchar(12) not null,
-    cidade varchar(200),
-    bairro varchar(200),
-    rua    varchar(200),
-    PRIMARY KEY (idMT, cpf, codigo_carteira)
- );
- 
- create table linha (
-	idLI int auto_increment primary key,
-    lin_idMT int,
-    lin_idONI int,
-	local_inicio varchar(200) NOT NULL,
-    local_fim varchar(200) NOT NULL,
-    horario_inicio datetime NOT NULL,
-    horario_fim  datetime NOT NULL,
-    constraint fk_motorista_linha foreign key (lin_idMT) references moto()
- );
+/* setimo esse*/
  
  create table viagem(
-	idLN int auto_increment primary key,
-	idLI int,
-    idMT int,
-    idONI int,
+	idVI int auto_increment primary key,
+	ln_idLI int,
+    vg_ln_idMT int,
+    vg_ln_idONI int,
     idCA int,
     nome_cliente varchar(200),
     cpf_cliente varchar(14),
 	data_entrada  date NOT NULL,
-    hora_entrada  time NOT NULL
+    hora_entrada  time NOT NULL,
+    constraint fk_viagem_linha_motorista foreign key(vg_ln_idLI) references linha(idLI),
+    constraint fk_viagem_linha_onibus foreign key (vg_ln_idONI) references linha(lin_idONI),
+    constraint fk_viagem_linha foreign key (ln_idLI) references linha(idLI),
+    constraint fk_viagem_clinte foreign key (idCA) references cartao(idCA)    
  );
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
+ /* oitavo esse*/
  
