82790c531270aaf4a26404c3281df4174075d7d1
ARTHYrobo <ARTHY.robot1@gmail.com>
2023-10-15 10:59:07 -0300

    criação do migrations

--- backend/backend/app.js
+++ backend/backend/app.js
@@ -1,13 +1,19 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
 
-var app = express();
+const dotenv = require("dotenv");
+dotenv.config({path: './.env'}); //lê o arquivo .env na inicialiazaçao.
+const cors = require('cors');
+
+const app = express();
+//configurar cors para liberar acessos como nesse caso onde irá ocorrer de onde ele ira ser aberto no navegador
+app.use(cors({origin: process.env.CLIENT_ORIGIN_URL}))
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -20,7 +26,8 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/api', usersRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
