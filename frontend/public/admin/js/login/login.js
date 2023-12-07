82235c457a7a45678c60d70e1c260895f7af6119
Clemerson Santos <clemerson.santos@aluno.ifsp.edu.br>
2023-12-07 00:12:22 -0300

    validação feedback

--- frontend/public/admin/js/login/login.js
+++ frontend/public/admin/js/login/login.js
@@ -4,12 +4,11 @@ document.addEventListener("DOMContentLoaded", () => {
 
     form.addEventListener("submit", async (event) => {
         event.preventDefault();
-
+        form.classList.add("was-validated")
         if (form.checkValidity()) {
             const email = document.querySelector("#email").value;
             const senha = document.querySelector("#senha").value;
             const data = { email, senha };
-
             try {
                 const response = await axios.post('http://localhost:3000/api/usuario/login', data);
                 const token = response.data.token;
@@ -17,9 +16,11 @@ document.addEventListener("DOMContentLoaded", () => {
                 window.location.href = `http://localhost:3001/admin/`
             } catch (error) {
                 console.error("danger", error.message);
+                form.classList.remove("was-validated")
+                document.querySelector("#email").classList.add("is-invalid")
+                document.querySelector("#senha").classList.add("is-invalid")
+                document.querySelector("#erro").innerHTML = "Email ou Senha Inválido"
             }
         }
-
-        form.classList.add("was-validated");
     });
 });
